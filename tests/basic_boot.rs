9ecfee5c6b82c76733b76ecab04fb486b3fe8aec
gtaha230812 <159329966+gtaha23@users.noreply.github.com>
2025-03-01 13:02:56 +0300

    Create basic_boot.rs

---
+++ tests/basic_boot.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(mos_rust::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use mos_rust::println;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    mos_rust::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn test_println() {
+	println!("test_println output");
+}
